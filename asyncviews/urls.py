f317267d8c6cf98250425675ad960f2d0a0142d2
Thiago Souza <thiago.souza@azos.com.br>
2024-09-06 08:20:16 -0300

    requirements

--- asyncviews/urls.py
+++ asyncviews/urls.py
@@ -20,5 +20,5 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', views.async_views),
+    path('api/', views.async_views), 
 ]
--- asyncviews/views.py
+++ asyncviews/views.py
@@ -2,17 +2,18 @@ import asyncio
 import httpx
 from django.http import JsonResponse
 
-
 async def http_call_async():
     for num in range(1, 6):
         await asyncio.sleep(1)
         print(f"Request {num}")
-        
+
     async with httpx.AsyncClient() as client:
         response = await client.get("https://jsonplaceholder.typicode.com/posts/1")
         print(f"Response {num}: {response.status_code}")
-        
+
 async def async_views(request):
     loop = asyncio.get_event_loop()
     loop.create_task(http_call_async())
-    return JsonResponse({"Non-blocking": "Response"}) 
+   
+    return JsonResponse({"Non-blocking": "Response"})
+
